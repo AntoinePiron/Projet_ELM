fcf53df4d51693ef3ac7a4db819c81d7837a6b18
AntoinePiron <pro.piron.antoine@gmail.com>
2021-12-23 14:01:52 +0100

    feat(global): :sparkles: Adding custom types

--- src/DrawingZone.elm
+++ src/DrawingZone.elm
@@ -1,1 +1,17 @@
-module DrawingZone exposing(..)
+module DrawingZone exposing (..)
+
+type Inst
+    = Forward Int
+    | Left Int
+    | Right Int
+    | Repeat Int
+
+type Prog = List Inst
+type alias Cursor = 
+    { x : Float
+    , y : Float
+    , a : Float
+    }
+
+printSvgTail : String
+printSvgTail = "</svg>"
